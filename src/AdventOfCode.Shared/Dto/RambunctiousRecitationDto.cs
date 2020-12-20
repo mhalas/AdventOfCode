735716f60bb5b160ce5c0b5d692d150aafe145eb
Maciej <maciejhalas@outlook.com>
2020-12-20 15:05:14 +0100

    Complete day 15.

---
+++ src/AdventOfCode.Shared/Dto/RambunctiousRecitationDto.cs
@@ -0,0 +1,8 @@
+﻿namespace AdventOfCode.Shared.Dto
+{
+    public class RambunctiousRecitationDto
+    {
+        public long Turn { get; set; }
+        public long Count { get; set; }
+    }
+}
---
+++ src/AdventOfCode.Tasks/Year2020/Day15_RambunctiousRecitation.cs
@@ -0,0 +1,56 @@
+﻿using AdventOfCode.Shared.Contracts;
+using AdventOfCode.Shared.Dto;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace AdventOfCode.Tasks.Year2020
+{
+    public class Day15_RambunctiousRecitation : IAdventTask
+    {
+        public Day15_RambunctiousRecitation()
+        {
+
+        }
+
+        public Task<string> Execute(IEnumerable<string> parameters)
+        {
+            var part2 = false;
+            var data = parameters.First().Split(',').Select(x=> long.Parse(x)).ToList();
+
+            var dinnerTime = (long)2020;
+            if (parameters.Count() == 2)
+                dinnerTime = long.Parse(parameters.ElementAt(1));
+
+            if (parameters.Count() == 2 && bool.TryParse(parameters.ElementAt(1), out bool isPart2))
+                part2 = isPart2;
+
+            var numbers = data.ToDictionary(x => x, x => new RambunctiousRecitationDto() { Turn = (long)data.IndexOf(x) + 1, Count = 1 });
+
+            var turn = data.Count();
+            var lastNumber = numbers.Last().Key;
+
+            numbers.Remove(lastNumber);
+            do
+            {
+                if (!numbers.ContainsKey(lastNumber))
+                {
+                    numbers.Add(lastNumber, new RambunctiousRecitationDto() { Turn = turn, Count = 1 });
+                    lastNumber = 0;
+                    continue;
+                }
+
+                var nextValue = turn - numbers[lastNumber].Turn;
+
+                numbers[lastNumber].Count++;
+                numbers[lastNumber].Turn = turn;
+
+                lastNumber = nextValue;
+            }
+            while(turn++ < dinnerTime - 1);
+
+            return Task.FromResult(lastNumber.ToString());
+        }
+    }
+}
