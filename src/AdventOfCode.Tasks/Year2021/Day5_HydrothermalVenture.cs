a4245c5f821b56555b8c17d29db96d78d43e01b5
Maciej Hałas <maciejhalas@outlook.com>
2021-12-08 17:38:35 +0100

    End day 5 2021

---
+++ src/AdventOfCode.Tasks/Year2021/Day5_HydrothermalVenture.cs
@@ -0,0 +1,30 @@
+﻿using AdventOfCode.Shared.Contracts;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace AdventOfCode.Tasks.Year2021
+{
+    public class Day5_HydrothermalVenture : IAdventTask
+    {
+        private readonly IReadListFromFile _readListFromFile;
+
+        public Day5_HydrothermalVenture(IReadListFromFile readListFromFile)
+        {
+            _readListFromFile = readListFromFile;
+        }
+
+        public Task<string> Execute(IEnumerable<string> parameters)
+        {
+            var part2 = false;
+            var data = _readListFromFile.ReadFile(parameters.First()).ToList();
+
+            if (parameters.Count() == 2 && bool.TryParse(parameters.ElementAt(1), out bool isPart2))
+                part2 = isPart2;
+
+            
+            return Task.FromResult(string.Empty);
+        }
+    }
+}
