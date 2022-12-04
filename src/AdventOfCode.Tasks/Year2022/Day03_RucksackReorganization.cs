83d44dbe01055dc99923c0618b58fd3c56189fee
Maciej Hałas <maciejhalas@outlook.com>
2022-12-04 14:10:56 +0100

    [Year2022] Finish day 03

---
+++ src/AdventOfCode.Tasks/Year2022/Day03_RucksackReorganization.cs
@@ -0,0 +1,58 @@
+﻿using AdventOfCode.Shared.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace AdventOfCode.Tasks.Year2022
+{
+    public class Day03_RucksackReorganization : IAdventTask
+    {
+        private readonly IReadListFromFile _readListFromFile;
+
+        private const int AsciiCodeUppercaseStart = 65;
+        private const int AsciiCodeLowercaseStart = 97;
+
+        public Day03_RucksackReorganization(IReadListFromFile readListFromFile)
+        {
+            _readListFromFile = readListFromFile;
+        }
+
+        public Task<string> Execute(IEnumerable<string> parameters)
+        {
+            var chunkSize = 1;
+            var data = _readListFromFile.ReadFile(parameters.First()).ToList();
+
+            if (parameters.Count() == 2 && int.TryParse(parameters.ElementAt(1), out int size))
+                chunkSize = size;
+
+            IEnumerable<IEnumerable<string>> xx = null;
+
+            if(chunkSize == 1)
+            {
+                xx = data
+                    .Select(x => x.Chunk((int)x.Length / 2).Select(x => string.Join("", x)));
+            }
+            else
+            {
+                xx = data
+                    .Chunk(chunkSize)
+                    .Select(x => x.AsEnumerable());
+            }
+
+            var result = xx
+                .SelectMany(x => 
+                    x.Aggregate((string string1, string string2) => 
+                        { 
+                            return string.Join("", string1.Intersect(string2)); 
+                        }).ToArray())
+                .Select(x => (int)x >= AsciiCodeLowercaseStart ? 
+                    ((int)x) - AsciiCodeLowercaseStart + 1 : ((int)x) - AsciiCodeUppercaseStart + 27)
+                .Sum();
+
+            return Task.FromResult(result.ToString());
+        }
+    }
+}
---
+++ src/AdventOfCode.Tests/Year2022/Day03_RucksackReorganizationTests.cs
@@ -0,0 +1,54 @@
+﻿using AdventOfCode.Shared.Contracts;
+using AdventOfCode.Tasks.Year2022;
+using NSubstitute;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace AdventOfCode.Tests.Year2022
+{
+    [TestFixture]
+    public class Day03_RucksackReorganizationTests
+    {
+        [Test]
+        public void Day03_RucksackReorganizationTests_Part1()
+        {
+            IEnumerable<string> inputs = new List<string>()
+            {
+                "vJrwpWtwJgWrhcsFMMfFFhFp",
+                "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL",
+                "PmmdzqPrVvPwwTWBwg",
+                "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn",
+                "ttgJtRGJQctTZtZT",
+                "CrZsJsPPZsGzwwsLwLmpwMDw",
+            };
+            var readListFromFile = Substitute.For<IReadListFromFile>();
+            readListFromFile.ReadFile("").Returns(inputs);
+
+            var task = new Day03_RucksackReorganization(readListFromFile);
+
+            var result = task.Execute(new List<string> { "" }).Result;
+            Assert.AreEqual("157", result);
+        }
+
+        [Test]
+        public void Day03_RucksackReorganizationTests_Part2()
+        {
+            IEnumerable<string> inputs = new List<string>()
+            {
+                "vJrwpWtwJgWrhcsFMMfFFhFp",
+                "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL",
+                "PmmdzqPrVvPwwTWBwg",
+                "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn",
+                "ttgJtRGJQctTZtZT",
+                "CrZsJsPPZsGzwwsLwLmpwMDw",
+            };
+            var readListFromFile = Substitute.For<IReadListFromFile>();
+            readListFromFile.ReadFile("").Returns(inputs);
+
+            var task = new Day03_RucksackReorganization(readListFromFile);
+
+            var result = task.Execute(new List<string> { "", "3" }).Result;
+            Assert.AreEqual("70", result);
+        }
+    }
+}
