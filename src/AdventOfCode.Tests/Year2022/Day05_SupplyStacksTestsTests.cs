ffa862dc16a334e7dfbdb7197ac8349096bd45c4
Maciej Hałas <maciejhalas@outlook.com>
2022-12-07 22:11:04 +0100

    Finish day 5 year 2022

---
+++ src/AdventOfCode.Tests/Year2022/Day05_SupplyStacksTestsTests.cs
@@ -0,0 +1,60 @@
+﻿using AdventOfCode.Shared.Contracts;
+using AdventOfCode.Tasks.Year2022;
+using NSubstitute;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace AdventOfCode.Tests.Year2022
+{
+    [TestFixture]
+    public class Day05_SupplyStacksTestsTestsTests
+    {
+        [Test]
+        public void Day05_SupplyStacksTestsTests_Part1()
+        {
+            IEnumerable<string> inputs = new List<string>()
+            {
+                "    [D]    ",
+                "[N] [C]    ",
+                "[Z] [M] [P]",
+                " 1   2   3 ",
+                "",
+                "move 1 from 2 to 1",
+                "move 3 from 1 to 3",
+                "move 2 from 2 to 1",
+                "move 1 from 1 to 2",
+            };
+            var readListFromFile = Substitute.For<IReadListFromFile>();
+            readListFromFile.ReadFile("").Returns(inputs);
+
+            var task = new Day05_SupplyStacks(readListFromFile);
+
+            var result = task.Execute(new List<string> { "" }).Result;
+            Assert.AreEqual("CMZ", result);
+        }
+
+        [Test]
+        public void Day05_SupplyStacksTestsTests_Part2()
+        {
+            IEnumerable<string> inputs = new List<string>()
+            {
+                "    [D]    ",
+                "[N] [C]    ",
+                "[Z] [M] [P]",
+                " 1   2   3 ",
+                "",
+                "move 1 from 2 to 1",
+                "move 3 from 1 to 3",
+                "move 2 from 2 to 1",
+                "move 1 from 1 to 2",
+            };
+            var readListFromFile = Substitute.For<IReadListFromFile>();
+            readListFromFile.ReadFile("").Returns(inputs);
+
+            var task = new Day05_SupplyStacks(readListFromFile);
+
+            var result = task.Execute(new List<string> { "", "true" }).Result;
+            Assert.AreEqual("MCD", result);
+        }
+    }
+}
