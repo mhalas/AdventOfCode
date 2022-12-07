bb65c7d9599db6619187880ada96441575779852
Maciej Hałas <maciejhalas@outlook.com>
2022-12-07 22:11:04 +0100

    Finish day 5 year 2022

--- src/AdventOfCode.ConsoleApplication/DryIocContainer.cs
+++ src/AdventOfCode.ConsoleApplication/DryIocContainer.cs
@@ -1,8 +1,5 @@
 ﻿using AdventOfCode.Shared.Common;
 using AdventOfCode.Shared.Contracts;
-using AdventOfCode.Tasks.Year2020;
-using AdventOfCode.Tasks.Year2021;
-using AdventOfCode.Tasks.Year2022;
 using DryIoc;
 
 namespace AdventOfCode.ConsoleApplication
@@ -16,33 +13,34 @@ namespace AdventOfCode.ConsoleApplication
             ApplicationContainer = new Container();
 
             ApplicationContainer.Register<IReadListFromFile, ReadListFromFile>();
-            ApplicationContainer.Register<IAdventTask, Day01_ReportRepair>(serviceKey: "2020-01");
-            ApplicationContainer.Register<IAdventTask, Day02_PasswordPhilosophy>(serviceKey: "2020-02");
-            ApplicationContainer.Register<IAdventTask, Day03_TobogganTrajectory>(serviceKey: "2020-03");
-            ApplicationContainer.Register<IAdventTask, Day04_PassportProcessing>(serviceKey: "2020-04");
-            ApplicationContainer.Register<IAdventTask, Day05_BinaryBoarding>(serviceKey: "2020-05");
-            ApplicationContainer.Register<IAdventTask, Day06_CustomCustoms>(serviceKey: "2020-06");
-            ApplicationContainer.Register<IAdventTask, Day07_HandyHaversacks>(serviceKey: "2020-07");
-            ApplicationContainer.Register<IAdventTask, Day08_HandheldHalting>(serviceKey: "2020-08");
-            ApplicationContainer.Register<IAdventTask, Day09_EncodingError>(serviceKey: "2020-09");
-            ApplicationContainer.Register<IAdventTask, Day10_AdapterArray>(serviceKey: "2020-10");
-            ApplicationContainer.Register<IAdventTask, Day11_SeatingSystem>(serviceKey: "2020-11");
-            ApplicationContainer.Register<IAdventTask, Day12_RainRisk>(serviceKey: "2020-12");
-            ApplicationContainer.Register<IAdventTask, Day13_ShuttleSearch>(serviceKey: "2020-13");
-            ApplicationContainer.Register<IAdventTask, Day14_DockingData>(serviceKey: "2020-14");
-            ApplicationContainer.Register<IAdventTask, Day15_RambunctiousRecitation>(serviceKey: "2020-15");
-            ApplicationContainer.Register<IAdventTask, Day16_TicketTranslation>(serviceKey: "2020-16");
+            ApplicationContainer.Register<IAdventTask, Tasks.Year2020.Day01_ReportRepair>(serviceKey: "2020-01");
+            ApplicationContainer.Register<IAdventTask, Tasks.Year2020.Day02_PasswordPhilosophy>(serviceKey: "2020-02");
+            ApplicationContainer.Register<IAdventTask, Tasks.Year2020.Day03_TobogganTrajectory>(serviceKey: "2020-03");
+            ApplicationContainer.Register<IAdventTask, Tasks.Year2020.Day04_PassportProcessing>(serviceKey: "2020-04");
+            ApplicationContainer.Register<IAdventTask, Tasks.Year2020.Day05_BinaryBoarding>(serviceKey: "2020-05");
+            ApplicationContainer.Register<IAdventTask, Tasks.Year2020.Day06_CustomCustoms>(serviceKey: "2020-06");
+            ApplicationContainer.Register<IAdventTask, Tasks.Year2020.Day07_HandyHaversacks>(serviceKey: "2020-07");
+            ApplicationContainer.Register<IAdventTask, Tasks.Year2020.Day08_HandheldHalting>(serviceKey: "2020-08");
+            ApplicationContainer.Register<IAdventTask, Tasks.Year2020.Day09_EncodingError>(serviceKey: "2020-09");
+            ApplicationContainer.Register<IAdventTask, Tasks.Year2020.Day10_AdapterArray>(serviceKey: "2020-10");
+            ApplicationContainer.Register<IAdventTask, Tasks.Year2020.Day11_SeatingSystem>(serviceKey: "2020-11");
+            ApplicationContainer.Register<IAdventTask, Tasks.Year2020.Day12_RainRisk>(serviceKey: "2020-12");
+            ApplicationContainer.Register<IAdventTask, Tasks.Year2020.Day13_ShuttleSearch>(serviceKey: "2020-13");
+            ApplicationContainer.Register<IAdventTask, Tasks.Year2020.Day14_DockingData>(serviceKey: "2020-14");
+            ApplicationContainer.Register<IAdventTask, Tasks.Year2020.Day15_RambunctiousRecitation>(serviceKey: "2020-15");
+            ApplicationContainer.Register<IAdventTask, Tasks.Year2020.Day16_TicketTranslation>(serviceKey: "2020-16");
 
-            ApplicationContainer.Register<IAdventTask, Day1_SonarSweep>(serviceKey: "2021-01");
-            ApplicationContainer.Register<IAdventTask, Day02_Dive>(serviceKey: "2021-02");
-            ApplicationContainer.Register<IAdventTask, Day03_BinaryDiagnostic>(serviceKey: "2021-03");
-            ApplicationContainer.Register<IAdventTask, Day04_GiantSquid>(serviceKey: "2021-04");
-            ApplicationContainer.Register<IAdventTask, Day05_HydrothermalVenture>(serviceKey: "2021-05");
+            ApplicationContainer.Register<IAdventTask, Tasks.Year2021.Day1_SonarSweep>(serviceKey: "2021-01");
+            ApplicationContainer.Register<IAdventTask, Tasks.Year2021.Day02_Dive>(serviceKey: "2021-02");
+            ApplicationContainer.Register<IAdventTask, Tasks.Year2021.Day03_BinaryDiagnostic>(serviceKey: "2021-03");
+            ApplicationContainer.Register<IAdventTask, Tasks.Year2021.Day04_GiantSquid>(serviceKey: "2021-04");
+            ApplicationContainer.Register<IAdventTask, Tasks.Year2021.Day05_HydrothermalVenture>(serviceKey: "2021-05");
 
-            ApplicationContainer.Register<IAdventTask, Day01_CalorieCounting>(serviceKey: "2022-01");
-            ApplicationContainer.Register<IAdventTask, Day02_RockPaperScissors>(serviceKey: "2022-02");
-            ApplicationContainer.Register<IAdventTask, Day03_RucksackReorganization>(serviceKey: "2022-03");
-            ApplicationContainer.Register<IAdventTask, Day04_CampCleanup>(serviceKey: "2022-04");
+            ApplicationContainer.Register<IAdventTask, Tasks.Year2022.Day01_CalorieCounting>(serviceKey: "2022-01");
+            ApplicationContainer.Register<IAdventTask, Tasks.Year2022.Day02_RockPaperScissors>(serviceKey: "2022-02");
+            ApplicationContainer.Register<IAdventTask, Tasks.Year2022.Day03_RucksackReorganization>(serviceKey: "2022-03");
+            ApplicationContainer.Register<IAdventTask, Tasks.Year2022.Day04_CampCleanup>(serviceKey: "2022-04");
+            ApplicationContainer.Register<IAdventTask, Tasks.Year2022.Day05_SupplyStacks> (serviceKey: "2022-05");
         }
     }
 }
---
+++ src/AdventOfCode.Tasks/Year2022/Day05_SupplyStacks.cs
@@ -0,0 +1,156 @@
+﻿using AdventOfCode.Shared.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace AdventOfCode.Tasks.Year2022
+{
+    public class Day05_SupplyStacks : IAdventTask
+    {
+        private const int MoveCountIndex = 0;
+        private const int FromIndex = 1;
+        private const int ToIndex = 2;
+
+        private readonly IReadListFromFile _readListFromFile;
+
+        public Day05_SupplyStacks(IReadListFromFile readListFromFile)
+        {
+            _readListFromFile = readListFromFile;
+        }
+
+        public Task<string> Execute(IEnumerable<string> parameters)
+        {
+            var part2 = false;
+            var data = _readListFromFile.ReadFile(parameters.First()).ToList();
+
+            if (parameters.Count() == 2 && bool.TryParse(parameters.ElementAt(1), out bool isPart2))
+                part2 = isPart2;
+
+            var emptySpaceIndex = data.IndexOf(string.Empty);
+
+            var stacks = GetStacks(data, emptySpaceIndex);
+            var actions = GetActions(data, emptySpaceIndex);
+
+            foreach (var action in actions)
+            {
+                if (part2)
+                {
+                    MoveCratesThroughStack(stacks, action);
+                }
+                else
+                {
+                    MoveCratesThroughQueue(stacks, action);
+                }
+            }
+
+            string result = string.Empty;
+            stacks.ForEach(stack =>
+            {
+                char crate;
+                if (stack.TryPeek(out crate))
+                {
+                    result += crate;
+                }
+            });
+
+            return Task.FromResult(result);
+        }
+
+        private static void MoveCratesThroughQueue(List<Stack<char>> stacks, int[] action)
+        {
+            var popedCrates = new Queue<char>();
+
+            for (int i = 0; i < action[MoveCountIndex]; i++)
+            {
+                char crate;
+
+                if (stacks[action[FromIndex] - 1].TryPop(out crate))
+                {
+                    popedCrates.Enqueue(crate);
+                }
+            }
+
+            foreach (var crate in popedCrates)
+            {
+                stacks[action[ToIndex] - 1].Push(crate);
+            }
+        }
+
+        private static void MoveCratesThroughStack(List<Stack<char>> stacks, int[] action)
+        {
+            var popedCrates = new Stack<char>();
+
+            for (int i = 0; i < action[MoveCountIndex]; i++)
+            {
+                char crate;
+
+                if (stacks[action[FromIndex] - 1].TryPop(out crate))
+                {
+                    popedCrates.Push(crate);
+                }
+            }
+
+            foreach (var crate in popedCrates)
+            {
+                stacks[action[ToIndex] - 1].Push(crate);
+            }
+        }
+
+        private List<int[]> GetActions(List<string> data, int emptySpaceIndex)
+        {
+            return data.GetRange(emptySpaceIndex + 1, data.Count() - emptySpaceIndex - 1)
+                .Select(x => x
+                    .Replace("move ", "")
+                    .Split(" from ")
+                    .SelectMany(y => y.Split(" to "))
+                    .Select(y => int.Parse(y.ToString()))
+                    .ToArray())
+                .ToList();
+        }
+
+        private static List<Stack<char>> GetStacks(List<string> data, int emptySpaceIndex)
+        {
+            List<Stack<char>> stackList = new List<Stack<char>>();
+
+            var mapRange = data
+                .GetRange(0, emptySpaceIndex);
+
+            mapRange.RemoveAt(mapRange.Count - 1);
+
+            var map = mapRange
+                .Chunk(4)
+                .SelectMany(x => x
+                    .Select(y => y
+                        .Replace("    ", " ")
+                        .Replace("[", "")
+                        .Replace("] ", "")
+                        .Replace("]", "")))
+                .Reverse()
+                .ToList();
+
+            for (int i = 0; i < map.Count(); i++)
+            {
+                var row = map[i];
+
+                for (int j = 0; j < row.Length; j++)
+                {
+                    if (stackList.Count < j + 1)
+                    {
+                        stackList.Add(new Stack<char>());
+                    }
+
+                    if (string.IsNullOrWhiteSpace(row[j].ToString()))
+                    {
+                        continue;
+                    }
+
+                    stackList[j].Push(row[j]);
+                }
+            }
+
+            return stackList;
+        }
+    }
+}
