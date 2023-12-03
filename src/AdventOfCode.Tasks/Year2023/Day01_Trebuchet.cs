c0b6b43540f1c479a84231f4119cae7bd343132f
mhalas <maciejhalas@outlook.com>
2023-12-03 17:24:48 +0100

    Finish day 1 year 2023.

---
+++ src/AdventOfCode.Tasks/Year2023/Day01_Trebuchet.cs
@@ -0,0 +1,104 @@
+﻿using AdventOfCode.Shared.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace AdventOfCode.Tasks.Year2023
+{
+    public class Day01_Trebuchet : IAdventTask
+    {
+        private readonly IReadListFromFile _readListFromFile;
+
+        private readonly IDictionary<string, int> _intDict = new Dictionary<string, int>()
+        {
+            { "one", 1 }, 
+            { "two", 2 }, 
+            { "three", 3 }, 
+            { "four", 4 }, 
+            { "five", 5 },
+            { "six", 6 }, 
+            { "seven", 7 }, 
+            { "eight", 8 }, 
+            { "nine", 9 }, 
+            { "1", 1 },
+            { "2", 2 },
+            { "3", 3 },
+            { "4", 4 },
+            { "5", 5 },
+            { "6", 6 },
+            { "7", 7 },
+            { "8", 8 },
+            { "9", 9 },
+        };
+
+        public Day01_Trebuchet(IReadListFromFile readListFromFile)
+        {
+            _readListFromFile = readListFromFile;
+        }
+
+        public Task<string> Execute(IEnumerable<string> parameters)
+        {
+            var part2 = false;
+            var data = _readListFromFile.ReadFile(parameters.First()).ToList();
+
+            if (parameters.Count() == 2 && bool.TryParse(parameters.ElementAt(1), out bool isPart2))
+                part2 = isPart2;
+
+            if(part2)
+            {
+                return ResultOfPart2(data);
+            }
+
+            return ResultOfPart1(data);
+        }
+
+        private Task<string> ResultOfPart1(List<string> data)
+        {
+            var result = data
+                .Select(x => x.Select(y => int.TryParse(y.ToString(), out int number) ? y : (char?)null))
+                .Select(x => x.Where(y => y.HasValue))
+                .Select(x => int.Parse($"{x.First()}{x.Last()}"))
+                .Sum()
+                .ToString();
+
+            return Task.FromResult(result);
+        }
+
+        private Task<string> ResultOfPart2(List<string> data)
+        {
+            var sum = 0;
+
+            foreach (var row in data)
+            {
+                var numbers = GetIndexNumberPairs(row);
+
+                sum += (numbers.First().Value * 10 + numbers.Last().Value);
+            }
+
+            return Task.FromResult(sum.ToString());
+        }
+
+        private IDictionary<int, int> GetIndexNumberPairs(string row)
+        {
+            var result = new Dictionary<int, int>();
+
+            foreach(var pair in _intDict)
+            {
+                var index = row.IndexOf(pair.Key);
+
+                if(index >= 0)
+                {
+                    result.Add(index, pair.Value);
+                }
+            }
+
+            return result
+                .OrderBy(x=>x.Key)
+                .ToDictionary(x => x.Key, x => x.Value);
+        }
+    }
+}
---
+++ src/AdventOfCode.Tests/Year2023/Day01_TrebuchetTests.cs
@@ -0,0 +1,55 @@
+﻿using AdventOfCode.Shared.Contracts;
+using AdventOfCode.Tasks.Year2023;
+using NSubstitute;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace AdventOfCode.Tests.Year2023
+{
+    [TestFixture]
+    public class Day01_TrebuchetTests
+    {
+        [Test]
+        public void Day01_TrebuchetTests_Part1()
+        {
+            IEnumerable<string> inputs = new List<string>()
+            {
+                "1abc2",
+                "pqr3stu8vwx",
+                "a1b2c3d4e5f",
+                "treb7uchet",
+            };
+            var readListFromFile = Substitute.For<IReadListFromFile>();
+            readListFromFile.ReadFile("").Returns(inputs);
+
+            var task = new Day01_Trebuchet(readListFromFile);
+
+            var result = task.Execute(new List<string> { "" }).Result;
+            Assert.AreEqual("142", result);
+        }
+
+        [Test]
+        public void Day01_TrebuchetTests_Part2()
+        {
+            IEnumerable<string> inputs = new List<string>()
+            {
+                "two1nine",
+                "eightwothree",
+                "abcone2threexyz",
+                "xtwone3four",
+                "4nineeightseven2",
+                "zoneight234",
+                "7pqrstsixteen",
+
+                "eightwo"
+            };
+            var readListFromFile = Substitute.For<IReadListFromFile>();
+            readListFromFile.ReadFile("").Returns(inputs);
+
+            var task = new Day01_Trebuchet(readListFromFile);
+
+            var result = task.Execute(new List<string> { "", "true" }).Result;
+            Assert.AreEqual("363", result);
+        }
+    }
+}
