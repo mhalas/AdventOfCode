ecca1eac0defbb6a96e892343c99ffa4f13a8264
mhalas <maciejhalas@outlook.com>
2023-12-11 00:11:37 +0100

    Add more tests to day 1 year 2023

--- src/AdventOfCode.Tests/Year2023/Day01_TrebuchetTests.cs
+++ src/AdventOfCode.Tests/Year2023/Day01_TrebuchetTests.cs
@@ -40,8 +40,6 @@ namespace AdventOfCode.Tests.Year2023
                 "4nineeightseven2",
                 "zoneight234",
                 "7pqrstsixteen",
-
-                "eightwo"
             };
             var readListFromFile = Substitute.For<IReadListFromFile>();
             readListFromFile.ReadFile("").Returns(inputs);
@@ -49,7 +47,64 @@ namespace AdventOfCode.Tests.Year2023
             var task = new Day01_Trebuchet(readListFromFile);
 
             var result = task.Execute(new List<string> { "", "true" }).Result;
-            Assert.AreEqual("363", result);
+            Assert.AreEqual("281", result);
+        }
+
+        [TestCase("34", 34)]
+        [TestCase("eightwo", 82)]
+        [TestCase("eightwothree", 83)]
+
+        [TestCase("1one", 11)]
+        [TestCase("2two", 22)]
+        [TestCase("3three", 33)]
+        [TestCase("4four", 44)]
+        [TestCase("5five", 55)]
+        [TestCase("6six", 66)]
+        [TestCase("7seven", 77)]
+        [TestCase("8eight", 88)]
+        [TestCase("9nine", 99)]
+
+        [TestCase("one1", 11)]
+        [TestCase("two2", 22)]
+        [TestCase("three3", 33)]
+        [TestCase("four4", 44)]
+        [TestCase("five5", 55)]
+        [TestCase("six6", 66)]
+        [TestCase("seven7", 77)]
+        [TestCase("eight8", 88)]
+        [TestCase("nine9", 99)]
+
+        [TestCase("1", 11)]
+        [TestCase("2", 22)]
+        [TestCase("3", 33)]
+        [TestCase("4", 44)]
+        [TestCase("5", 55)]
+        [TestCase("6", 66)]
+        [TestCase("7", 77)]
+        [TestCase("8", 88)]
+        [TestCase("9", 99)]
+        [TestCase("one", 11)]
+        [TestCase("two", 22)]
+        [TestCase("three", 33)]
+        [TestCase("four", 44)]
+        [TestCase("five", 55)]
+        [TestCase("six", 66)]
+        [TestCase("seven", 77)]
+        [TestCase("eight", 88)]
+        [TestCase("nine", 99)]
+        public void Day01_TrebuchetTests_CheckSingleRows(string input, int expectedResult)
+        {
+            IEnumerable<string> inputs = new List<string>()
+            {
+                input
+            };
+            var readListFromFile = Substitute.For<IReadListFromFile>();
+            readListFromFile.ReadFile("").Returns(inputs);
+
+            var task = new Day01_Trebuchet(readListFromFile);
+
+            var result = task.Execute(new List<string> { "", "true" }).Result;
+            Assert.AreEqual(expectedResult.ToString(), result);
         }
     }
 }
